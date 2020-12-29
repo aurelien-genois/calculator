c6c601b63e8b9d7953fe5e9aee638bdcc20b2692
Smogo <aurelien.genois@gmail.com>
2020-12-29 12:48:40 +0100

    initiate basic logic of operations

---
+++ logic.js
@@ -0,0 +1,26 @@
+function add(...a) {
+    return a.reduce((result, number) => result + number);
+}
+function substract(...a) {
+    return a.reduce((result, number) => result - number);
+}
+function multiply(...a) {
+    return a.reduce((result, number) => result * number);
+}
+function divide(...a) {
+    return a.reduce((result, number) => result / number);
+}
+
+function operate(operator,a,b) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return substract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
+// need to have "" around the operator (or a string) : operate("+",3,4)
